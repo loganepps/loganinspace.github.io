5a4475356fdb7dfa83dcb77d27ce20b6e5413c54
Logan Epps <logepps@gmail.com>
2020-07-27 18:26:34 -0500

    My first commit.
    To-do List Project

---
+++ TodoListProject/assets/js/todos.js
@@ -0,0 +1,33 @@
+//Check off specific todos by clicking
+//Use .on instead of .click to add event listeners to elements that exist when page loads so we can account for elements that aren't there yet
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("completed");
+});
+
+//Click on X to delete Todo
+$("ul").on("click", "span", function(event){
+    //Use .parent() to retrieve li enclosing the clicked span
+    //Pass in callback function to manipulate fadeout duration and run .remove() after that
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+    //Stops event from bubbling up to other elements
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        //Extract new todo text from input 
+        var todoText = ($(this).val());
+        //Clear input
+        $(this).val("");
+        //Creat new li and add to ul
+        //Use .append to take a string of html and append elements to whatever is selected
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
+    }
+});
+
+$(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle();
+    $("input[type='text']").removeClass();
+})
